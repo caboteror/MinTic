ebd084d92637796223ab87406185cf8808f36196
Carlos Alberto Botero Restrepo <caboteror@gmail.com>
2022-07-26 21:01:55 -0500

    Add files via upload

---
+++ PracticaReto5/fresas/beans/Fresa.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fresas.beans;
+
+/**
+ *
+ * @author cbotero
+ */
+public class Fresa {
+    private int id;
+    private String familia;
+    private int precio;
+    private int anio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(String familia) {
+        this.familia = familia;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
+    
+}
